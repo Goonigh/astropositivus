9c71512722e2c325ee8480b25339ec4b531ef004
Nay Gozalova <mondayflows@Nays-MacBook-Pro.local>
2026-02-15 23:32:36 +0400

    feat: add node adapter for coolify ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,14 @@ import tailwind from "@astrojs/tailwind";
 // Import the Vercel adapter
 import vercel from '@astrojs/vercel';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://positivustheme.vercel.app",
   integrations: [tailwind()],
   output: 'server',
-  adapter: vercel(),
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
